50c209e005ca2f4e3aa315cda6f9eb5c0286f764
macyjdraper <macyj93@att.net>
2019-06-24 20:53:16 -0500

    Spell check

--- TextAdventure/Program.cs
+++ TextAdventure/Program.cs
@@ -14,7 +14,7 @@ namespace TextAdventure
             Console.WriteLine("Where had she seen those beautiful eyes before,");
             Console.WriteLine("she wondered...");
             Console.WriteLine("on her way back to the car she sees the girl with beautiful eyes");
-            Console.WriteLine("Look at all of those grocieres she thinks to her self");
+            Console.WriteLine("Look at all of those groceries she thinks to her self");
             Console.WriteLine("Those are so many groceries...");
             Console.WriteLine("She screamed out");
             Console.WriteLine("Oh Dear, would you like some help with your groceries?");
@@ -50,24 +50,24 @@ namespace TextAdventure
                 Console.WriteLine("Great we will meet tomorrow at ten"); 
             }
             Console.WriteLine("The very next day the plain women was waiting");
-            Console.WriteLine("to meet " + name + " at the coffee shope.");
+            Console.WriteLine("to meet " + name + " at the coffee shop.");
             Console.WriteLine("For some reason she was so intrigued by this beautiful girl.");
-            Console.WriteLine("She grabed them two coffees as she waited on " + name);
-            Console.WriteLine(name + " finaly walked in");
+            Console.WriteLine("She grabbed them two coffees as she waited on " + name);
+            Console.WriteLine(name + " finally walked in");
             Console.WriteLine("Hey " + name + " come sit over here.");
-            Console.WriteLine("They started to talk about life in " + home + "the music, the culture and the history");
+            Console.WriteLine("They started to talk about life in " + home + " the music, the culture and the history");
             Console.WriteLine("How old are you the plain women asked?");
             string age = Console.ReadLine();
             Console.WriteLine("Wooowww the last time I visited " + home + " was " + age + " years ago!");
-            Console.WriteLine("It's a very sad story i dont normaly talk about");
-            Console.WriteLine("But if you would like to hear it I will tell you?");
+            Console.WriteLine("It's a very sad story i don't normally talk about,");
+            Console.WriteLine("but if you would like to hear it I will tell you?");
 
             string hearStory = Console.ReadLine();
             if ( hearStory == "yes")
             {
                 Console.WriteLine(age + " years ago i visited " + home + " and met a very beautiful man,");
                 Console.WriteLine("with very beautiful eyes.");
-                Console.WriteLine("Actualy they were very simialar to yours.");
+                Console.WriteLine("Actually they were very similar to yours.");
                 Console.WriteLine("His name was Rafale.");
             }
             Console.WriteLine("Wooww thats my fathers name said " + name);
@@ -80,10 +80,10 @@ namespace TextAdventure
             Console.WriteLine("At that time I was only 18 years old");
             Console.WriteLine("with no job no money and no way to support her");
             Console.WriteLine("I made the decision to leave her with her father");
-            Console.WriteLine("I had no buisness raising a child");
+            Console.WriteLine("I had no business raising a child");
             Console.WriteLine("When I was still a child myself");
-            Console.WriteLine("I never spoke to him agian but i still think about");
-            Console.WriteLine("The beautiful girl I gave up every day");
+            Console.WriteLine("I never spoke to him again but i still think about");
+            Console.WriteLine("The beautiful girl I gave up every day.");
             
         }
     }
